79e8ee8b8dba916fd50c5f4b4823101e25b7fb02
iraviteja <iraviteja77@gmail.com>
2018-07-08 18:37:16 +0530

    Adding title and description

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import SearchBar from "./components/Search_bar";
 import VideoList from "./components/Video_list";
+import VideoDetail from "./components/Video_detail";
 import config from "./config.json";
 
 const { API_KEY } = config;
@@ -11,16 +12,20 @@ const { API_KEY } = config;
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: "Planet Earth" }, data => {
-      this.setState({ videos: data });
+      this.setState({ videos: data, selectedVideo: data[0] });
     });
   }
   render() {
     return (
       <div className="container">
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
